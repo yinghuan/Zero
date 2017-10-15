0d5b0127ea86fe62cf9ad4efbe02b26231b1143c
yinghuan <yangzhen@bytedance.com>
2017-10-15 21:38:02 +0800

    adapter some zshell

--- make_fake_module.sh
+++ make_fake_module.sh
@@ -3,5 +3,6 @@
 rm -rf "${BUILT_PRODUCTS_DIR}/Headers"
 mkdir "${BUILT_PRODUCTS_DIR}/Headers"
 find "${BUILT_PRODUCTS_DIR}" -name "*.h" |xargs -I {} cp {} "${BUILT_PRODUCTS_DIR}/Headers"
-cp "$(dirname $SRCROOT)/modules/$PRODUCT_NAME-umbrella.h" "${BUILT_PRODUCTS_DIR}/Headers"
-cp "$(dirname $SRCROOT)/modules/module.modulemap" "${BUILT_PRODUCTS_DIR}/Headers"
+
+cp "$(dirname "$SRCROOT")/modules/$PRODUCT_NAME-umbrella.h" "${BUILT_PRODUCTS_DIR}/Headers"
+cp "$(dirname "$SRCROOT")/modules/module.modulemap" "${BUILT_PRODUCTS_DIR}/Headers"
